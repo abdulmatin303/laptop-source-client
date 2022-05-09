c50af4ae65e4f735895a47ddf7979eae1fb7c19c
amr303 <amr.abdulmatin@gmail.com>
2022-05-10 00:26:09 +0600

    add service data connect with server

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -4,7 +4,23 @@ import { useForm } from "react-hook-form";
 
 const AddService = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/service`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res=> res.json())
+        .then(result =>{
+            console.log(result);
+        } )
+    };
+
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Add a Service</h2>
